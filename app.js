e55f442eaec7f5febf616f7fabdc398a69e0259e
Ubuntu <ubuntu@ip-172-31-39-120.ap-northeast-1.compute.internal>
2018-09-21 09:00:54 +0000

    add logger module

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
 const jwt = require('koa-jwt');
 const errorHandle = require('./middlewares/errorHandle');
+const logger = require('koa-logger');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.connexionString);
 
 const app = new Koa();
+app.use(logger());
 // log request URL:
-app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-    await next();
-});
-
 app.use(errorHandle);
+
 app.use(jwt({
     secret:config.secret
 }).unless({
