d9e0e5cf50b271fd58846c8eadb650250e24c9f2
allen <277960419@qq.com>
2018-09-25 08:59:50 +0800

    modify iching response

--- controllers/iching.js
+++ controllers/iching.js
@@ -1,19 +1,24 @@
 var iChing = require("i-ching");
 
 var fn_iChing= async (ctx, next) => {
-    var name = ctx.params.name;
-    var reading = iChing.ask(name);
+    var question = ctx.params.question;
+    var reading = iChing.ask(question);
     var result = reading.hexagram.number + reading.hexagram.character + reading.hexagram.names.join(', ')
 
     console.log(result);
     console.log(typeof(result));
 
-    ctx.response.body = `<h1>Hello, ${reading.hexagram.number}, ${reading.hexagram.character}, ${reading.hexagram.names.join(', ')}!</h1>`;
+    // ctx.response.body = `<h1>Hello, ${reading.hexagram.number}, ${reading.hexagram.character}, ${reading.hexagram.names.join(', ')}!</h1>`;
+    ctx.response.body = {
+        number:reading.hexagram.number,
+        character:reading.hexagram.character,
+        names:reading.hexagram.names.join(', ')
+    };
 };
 
 
 
 module.exports = {
-    'GET /iching/:name': fn_iChing
+    'POST /iching': fn_iChing
 };
 
